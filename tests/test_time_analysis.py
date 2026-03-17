0b9e6a01e3424a5ebf7fabeb004dd054490f5088
HarshPatel285 <hsp498@gmail.com>
2026-03-17 00:17:29 -0400

    test(analytics): implement automated tests for analytics functions [US-58]

--- tests/test_time_analysis.py
+++ tests/test_time_analysis.py
@@ -1,7 +1,6 @@
 from src.data_loader import load_dataset
 from src.data_cleaning import clean_dataset
-from src.time_analysis import collisions_by_hour
-
+from src.time_analysis import collisions_by_hour, collisions_by_weekday
 
 def test_collisions_by_hour():
 
@@ -10,6 +9,17 @@ def test_collisions_by_hour():
     df = clean_dataset(df)
 
     result = collisions_by_hour(df)
+
+    assert result is not None
+    assert len(result) > 0
+
+def test_collisions_by_weekday():
+
+    df = load_dataset("data/Traffic_Collisions_Open_Data.csv")
+
+    df = clean_dataset(df)
+
+    result = collisions_by_weekday(df)
 
     assert result is not None
     assert len(result) > 0
